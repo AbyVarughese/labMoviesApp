f5bbc0779c1215d28ec916adb787a7a44c757e40
Aby Varughese <abyasnu@gmail.com>
2023-06-09 12:16:39 +0100

    New button for Upcoming movies page.

--- src/pages/upcoming.jsx
+++ src/pages/upcoming.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcoming } from "../api/tmdb-api";
 import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 
 const UpcomingMoviesPage = (props) => {
   const [movies, setMovies] = useState([]);
@@ -27,7 +28,9 @@ const UpcomingMoviesPage = (props) => {
       title="Discover Movies"
       movies={movies}
       action={(movie) => {
-        return <AddToFavouritesIcon movie={movie} />;
+        //return <AddToFavouritesIcon movie={movie} />;
+        return <PlaylistAddIcon movie={movie} />;
+        
       }}
     />
   );
